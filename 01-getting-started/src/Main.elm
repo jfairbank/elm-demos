9eedbed0bd0dcfcb9d53025aaff19b5706b6d485
Jeremy Fairbank <elpapapollo@gmail.com>
2017-01-25 10:25:35 -0500

    Tweaks to counter app

--- 01-getting-started/src/Main.elm
+++ 01-getting-started/src/Main.elm
@@ -1,58 +1,14 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Html.Attributes exposing (class)
-import Html.Events exposing (onClick)
-
-
-type alias Model =
-    Int
-
-
-type Msg
-    = Increment
-    | Decrement
-
-
-initialModel : Model
-initialModel =
-    0
-
-
-init : ( Model, Cmd Msg )
-init =
-    ( initialModel, Cmd.none )
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case msg of
-        Increment ->
-            ( model + 1, Cmd.none )
-
-        Decrement ->
-            ( model - 1, Cmd.none )
-
-
-view : Model -> Html Msg
-view model =
-    div [ class "counter-01" ]
-        [ div [] [ text (toString model) ]
-        , button [ onClick Increment ] [ text "Increment" ]
-        , button [ onClick Decrement ] [ text "Decrement" ]
-        ]
-
-
-subscriptions : Model -> Sub msg
-subscriptions model =
-    Sub.none
+import App exposing (..)
+import Html exposing (program)
 
 
 main : Program Never Model Msg
 main =
-    Html.program
-        { init = init
-        , view = view
+    program
+        { view = view
+        , init = init
         , update = update
         , subscriptions = subscriptions
         }
